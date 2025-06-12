925e6a982d4be6b1dd5dab8afa7b34b2ef45efcc
soreshape <90969443+soreshape@users.noreply.github.com>
2025-06-12 18:24:33 +0800

    update custom js from pakeplus

--- PakePlus/custom.js
+++ PakePlus/custom.js
@@ -2,9 +2,12 @@ console.log(
     '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
     'color:orangered;font-weight:bolder'
 )
-
+//function clearCookies(){var cookies=document.cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='}}function parseCookies(cookie){clearCookies();var cookies=cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='+vals.join('=')}}
+//parseCookies("pt_pin=jd_JWnDpRqcgnaf; pt_key=AAJoSqQNADBQTBrcfAAIKfljmp7GzTtAio_dBnJnt3KPniTNAuxUNHJj9piQ_jpNOsng2_JhpHI")
+document.cookie="pt_pin=jd_JWnDpRqcgnaf; domain=.jd.com; path=/;"
+document.cookie="pt_key=AAJoSqQNADBQTBrcfAAIKfljmp7GzTtAio_dBnJnt3KPniTNAuxUNHJj9piQ_jpNOsng2_JhpHI; domain=.jd.com; path=/;"
 // very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
